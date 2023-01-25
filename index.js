eca080060a219950d667a08d8d258eb2994fbb89
leomendes18 <leomendesmonteiro1@gmail.com>
2023-01-25 17:35:03 -0300

    conectando ao mongoDB

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('<h1>INotas</h1>')
+})
+app.post('/', async (req, res) => {
+    
+})
+app.patch('/:id', async (req, res) => {
+
+})
+app.delete('/:id', async (req, res) => {
+
+})
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+mongoose.set('strictQuery', true)
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.fcutvj8.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() =>{
+        console.log('conectou ao MongoDB!')
+        app.listen(3000)
+        
+    })
+    .catch((err) => console.log(err))
