b3d5888d085df141dad77d25aea05ac4c421b423
dxArtur <daniel.arturjsb@gmail.com>
2023-01-26 20:53:03 -0300

    route of creation

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
+const Note = require('./models/Note')
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/source/html/index.html');
-});
+    res.send('<h1>INotas</h1>')
+})
 
-app.post('/', async (req, res) => {
-    
-});
+
+app.post('/addnote', async (req, res) => {
+
+    const {title, content} = req.body
+
+    const note = {
+        title,
+        content,
+    }
+
+    try {
+        await Note.create(note)
+
+        res.status(201).json({message: 'Note inserted'})
+        //route of creation
+
+
+    } catch (error){
+        res.status(500).json({error: error})
+    }
+})
+
 
 app.patch('/:id', async (req, res) => {
 
-});
-
+})
 app.delete('/:id', async (req, res) => {
 
-});
+})
 
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_NAME = process.env.DB_NAME;
-const DB_CODE = process.env.DB_CODE;
-mongoose.set('strictQuery', true);
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_NAME}.${DB_CODE}.mongodb.net/?retryWrites=true&w=majority`)
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+mongoose.set('strictQuery', true)
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.fcutvj8.mongodb.net/?retryWrites=true&w=majority`)
     .then(() =>{
-        console.log('conectou ao MongoDB!');
-        app.listen(3000);
+        console.log('conectou ao MongoDB!')
+        app.listen(3000)
         
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(err))
+
+
+    const bttnAdd = document.getElementById('add');
+
+    //fazer um const que percorra o banco para procurar todas as notas e ja imprimir na tela
+
+    mongodb+srv://dxniel:<password>@cluster0.grivyap.mongodb.net/?retryWrites=true&w=majority
