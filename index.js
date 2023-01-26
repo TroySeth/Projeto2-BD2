d31244a67f6fd9fd04faf05ee119b9f6c93b4ec1
Guilherme Costa <gui-costaquino@hotmail.com>
2023-01-26 01:55:26 -0300

    Organizando arquivos em pastas, adicionando header ao html e estilizando

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.send('<h1>INotas</h1>')
-})
+    res.sendFile(__dirname + '/source/html/index.html');
+});
+
 app.post('/', async (req, res) => {
     
-})
+});
+
 app.patch('/:id', async (req, res) => {
 
-})
+});
+
 app.delete('/:id', async (req, res) => {
 
-})
+});
 
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-mongoose.set('strictQuery', true)
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+mongoose.set('strictQuery', true);
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.fcutvj8.mongodb.net/?retryWrites=true&w=majority`)
     .then(() =>{
-        console.log('conectou ao MongoDB!')
-        app.listen(3000)
+        console.log('conectou ao MongoDB!');
+        app.listen(3000);
         
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
