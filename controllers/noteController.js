8cbc12121dbdb1139e69e263edb5da43a44297d8
Guilherme Costa <gui-costaquino@hotmail.com>
2023-01-30 10:23:11 -0300

    Modificando tempo da função de tempo de espera

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -7,10 +7,10 @@ async function create (req, res){
             title: req.body.title,
             content: req.body.content,
             date: noteModel.date,
-        }).save()
+        }).save();
         setTimeout(function() {
             res.status(201).redirect('/');
-        }, 3000);
+        }, 2000);
     } catch(error){
         console.log("Erro ao criar nota:" + error);
     }
@@ -30,7 +30,7 @@ async function editNote (req, res){
         await noteModel.updateOne({_id: req.body.id},{title: req.body.title, content: req.body.content});
         setTimeout(function() {
             res.status(201).redirect('/');
-        }, 3000);
+        }, 1000);
     } catch(error){
         console.log("Falha ao editar nota: " + error);
     }
@@ -40,7 +40,7 @@ async function destroyNote (req, res){
         await noteModel.deleteOne({_id: req.body.id}).then(() => {
             setTimeout(function() {
                 res.status(200).redirect('/');
-            }, 3000);
+            }, 500);
     }).catch((error) => {
         console.log("Erro ao deletar nota." + error);
     }
